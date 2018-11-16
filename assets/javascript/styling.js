67dbeccd86e65e09d7fbe4e239dc2a05c7a86c0e
Aijune <43250155+AFilippis@users.noreply.github.com>
2018-11-15 22:07:13 -0500

    responsiveness updates

--- assets/javascript/styling.js
+++ assets/javascript/styling.js
@@ -1,6 +1,1 @@
-$( window ).resize(function() {
-    if($(window).width() <=768){
-    $('#media-query-break').addClass("line-break");
-    }
-    else $('#media-query-break').removeClass("line-break");
-  });
+// Enter page JS here:
